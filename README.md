65790b921a320901670460fdb479b82c44362d9d
jzalger <jzalger@gmail.com>
2015-06-01 22:08:20 -0400

    Added PWM Examples

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 ImagiSat
 ========
+ImagiSat is a meteorologist in your pocket. It delivers supercomputed weather forecasts for anywhere to anywhere.
 
-ImagiSat is a meteorologist in your pocket. It delivers supercomputed weather forecasts for anywhere to anywhere
+Hardware
+========
+The core of the ImagiSat is the Intel Edison Microcontroller. This is a powerful yet energy efficient X86 based
+chip with on-board WiFI and Bluetooth LE running a Linux kernel. It has a dual core 500MHz Atom processor, 4GB flash 
+storage, and 1GB ram.
+
+Software
+========
+All core logic is written in Python. 
+
+I2C Support
+-----------
+
+For i2C support using the Adafruit based I2C Python libraries, the following steps were required:
+1. Install i2c-tools and python-smbus on Edison Linux.
+2. Adafruit GPIO.I2C library was modified to return 1 as the bus when querying the device. Also require_repeated_start() 
+was bypassed as this was implemented for the RPI and does not seem necessary for the Edison.
+
+3. The modified Adafruit GPIO.I2C was installed, followed by the necessary I2C device modules.
---
+++ sandbox.py
@@ -0,0 +1,90 @@
+"""
+sandbox.py is a script for testing and prototyping ImagiSat functionality.
+"""
+__author__ = 'jzalger'
+
+import mraa
+import serial
+from pynmea import nmea
+
+# Read a GPIO Pin
+pin = mraa.Gpio(31)
+pin.dir(mraa.DIR_IN)
+pwrStatus = pin.read()
+
+##################################################
+# Serial
+##################################################
+# Query GPS
+gpsSerial = serial.Serial('/dev/ttyMFD1', 4800)  # MFD1 corresponds to UART1
+msg = gpsSerial.readline()  # Reads some characters
+
+# Parse the GPS coorginates.
+# Borrowed from https://www.sparkfun.com/tutorials/403
+
+gpgga = nmea.GPGGA()
+
+# Check if its a location message
+while True:
+    msg = gpsSerial.readline()
+    if 'GPGGA' in msg:
+        gpgga.parse(msg)
+
+        lats = gpgga.latitude
+        longs = gpgga.longitude
+
+        # convert degrees,decimal minutes to decimal degrees
+        lat1 = (float(lats[2]+lats[3]+lats[4]+lats[5]+lats[6]+lats[7]+lats[8]))/60
+        lat = (float(lats[0]+lats[1])+lat1)
+        long1 = (float(longs[3]+longs[4]+longs[5]+longs[6]+longs[7]+longs[8]+longs[9]))/60
+        long = (float(longs[0]+longs[1]+longs[2])+long1)
+
+        # Do something with this usefulness
+
+##################################################
+# PWM
+##################################################
+pwmPin = mraa.Pwm(21)  # MRAA pin number, eg 21 = PWM 3
+pwmPin.period_us(700)  # Define the duty cycle period in microseconds
+pwmPin.enable(True)    # Activate the pin
+pwmPin.write(0.5)      # Write value betwen 0 and 1
+pwmPin.enable(False)
+
+##################################################
+# I2C
+##################################################
+
+# BMP085 Sensor Data
+import Adafruit_BMP.BMP085 as BMP085
+sensor = BMP085.BMP085()
+sensor.read_temperature()
+sensor.read_pressure()
+
+# MPR121
+import Adafruit_MPR121.MPR121 as MPR121
+cap = MPR121.MPR121()
+if not cap.begin():
+    print "Badness"
+while True:
+    if cap.is_touched(5):
+        print "Awesome"
+
+# ADXL345 using UPM
+import pyupm_adxl345
+acclSensor = pyupm_adxl345.Adxl345(1) # 1 is the i2c bus number
+acclSensor.update()
+a = acclSensor.getAcceleration()  # Returns a list of floats [x, y, z]
+
+# HMC58831 Digital Compas
+import pyupm_hmc5883l
+compas = pyupm_hmc5883l.Hmc5881l(1)  # 1 is the i2c bus number
+compas.update()
+heading = compas.heading()
+direction = compas.direction()
+coordinates = compas.coordinates()  # list of ints [x, y, z] with
+
+# SHT 75
+import Sht1x
+sht1x = Sht1x.Sht1x(7, 19)       # Using the 3.3V I2C bus
+temp = sht1x.read_temperature_C()
+humidity = sht1x.read_humidity()
