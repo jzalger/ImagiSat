e6d0c5c1fcff98573a81178c50c4b958278521ce
jzalger <jzalger@users.noreply.github.com>
2022-10-08 23:02:13 -0400

    Changed partition table to maximize program space and enabled OTA libraries.

--- src/imagisat.ino
+++ src/imagisat.ino
@@ -2,15 +2,15 @@
 #include "Device.h"
 #include "types.h"
 
-//#include <esp32fota.h>
-//#include <WiFi.h>
+#include <esp32fota.h>
+#include <WiFi.h>
 
 const uint8_t current_firmware_version = 0;
 
 MainStateMachine mainStateMachine;
 
-//WiFiClientSecure ota_client;
-//esp32FOTA esp32FOTA("ImagiSat", current_firmware_version, false);
+WiFiClientSecure ota_client;
+esp32FOTA esp32FOTA("ImagiSat", current_firmware_version, false);
 
 void setup()
 {
